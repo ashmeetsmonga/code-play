582cdf3e4c04df2c8cc45045f1437c62bbf3cd8a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2022-12-25 17:41:52 +0530

    HTML, CSS, JS pane added in top pane

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
 
 function App() {
-	return <div className='App'>Code Play</div>;
+	const [html, setHtml] = useState("");
+	const [css, setCss] = useState("");
+	const [js, setJs] = useState("");
+
+	return (
+		<div className='container'>
+			<div className='pane pane-code'>
+				<Editor value={html} displayName='HTML' language='xml' onChange={setHtml} />
+				<Editor value={css} displayName='CSS' language='css' onChange={setCss} />
+				<Editor value={js} displayName='JS' language='javascript' onChange={setJs} />
+			</div>
+			<div className='pane pane-output'>Output</div>
+		</div>
+	);
 }
 
 export default App;
